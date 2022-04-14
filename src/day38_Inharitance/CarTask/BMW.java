90a1228216ee2aea75d38836636c93fc5de0cdf3
cholponai <cholpondzhumabeva@gmail.com>
2022-04-14 17:34:12 +0200

    day38: Inharitance Method Overriding

--- src/day38_Inharitance/CarTask/BMW.java
+++ src/day38_Inharitance/CarTask/BMW.java
@@ -1,5 +1,26 @@
 package day38_Inharitance.CarTask;
 
-public class BMW {
+public class BMW extends Car{
+
+
+    public BMW(String model, int year, String color, double price, int miles) {
+        super("BMW", model, year, color, price, miles);
+    }
+
+    public void breaksDowns(){
+        System.out.println(brand+" "+ model+ " is breaking down");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+ model+ " is racing");
+    }
+
+
 
 }
+/*
+2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+ */
--- src/day38_Inharitance/CarTask/Tesla.java
+++ src/day38_Inharitance/CarTask/Tesla.java
@@ -1,4 +1,14 @@
 package day38_Inharitance.CarTask;
 
-public class Tesla {
+public class Tesla extends Car{
+
+    public Tesla(String model, int year, String color, double price, int miles) {
+        super("Tesla", model, year, color, price, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand+ " "+ model+ " in autopilot mode");
+    }
+
 }
+
