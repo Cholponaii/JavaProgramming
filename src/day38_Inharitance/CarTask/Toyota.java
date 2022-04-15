51ab978c981b4f5b1c9b28ac25f96041254f5450
cholponai <cholpondzhumabeva@gmail.com>
2022-04-15 18:57:20 +0200

    day38: Inharitance Method Overriding

--- src/day38_Inharitance/CarTask/Toyota.java
+++ src/day38_Inharitance/CarTask/Toyota.java
@@ -12,6 +12,12 @@ public void reliable(){
     System.out.println(brand+ " "+ model+ " is reliable");
 }
 
+ public void start(){
+     System.out.println("Twist the key to ignition to start "+brand+model);
+ }
+
+
+
 
 
 }
