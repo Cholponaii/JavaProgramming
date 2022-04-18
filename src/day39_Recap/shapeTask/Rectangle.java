17d8512cedd29ec93e3410bcd5eaeb36678b243d
cholponai <cholpondzhumabeva@gmail.com>
2022-04-18 13:18:48 +0200

    day39: Recap

---
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,21 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle {
+
+
+}
+/*
+Rectangle extends Shape:
+	variables:
+		length
+		width
+
+	Encapsulate the fields
+
+	Add a constructor to set the filed
+
+	area(): length * width
+	perimeter(): 2 * ( width + length)
+	toString(): length, width, area, perimeter
+
+ */
