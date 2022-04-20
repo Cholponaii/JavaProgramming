9f81f33bb55272d18cf33421570595b8f27a69cc
cholponai <cholpondzhumabeva@gmail.com>
2022-04-20 20:25:35 +0200

    day40: Protected Acces Modifier & Final keyword

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,9 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo"; // default modifier
+    protected static String name2 = "Wooden Spoon"; // protected
+
+
+}
